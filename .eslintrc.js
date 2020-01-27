6f54b256f2847159d1fa01c5f106c81a01839b9f
Kevin Coulibaly <coulibaly.d.kevin@gmail.com>
2020-01-27 22:56:11 +0100

    tune eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,11 @@ module.exports = {
   extends: 'standard',
   rules: {
     'comma-dangle': ['error', 'only-multiline'],
+    'space-before-function-paren': ['off'],
   },
   globals: {
-    'describe': true,
-    'test': true,
-    'expect': true,
-  }
+    describe: true,
+    test: true,
+    expect: true,
+  },
 }
